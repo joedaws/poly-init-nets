cb624739d7f2f0d260846f76043133d0fdc16cd4
Joseph Daws <daws.joseph@gmail.com>
2019-07-11 18:03:29 -0400

    updated polynomial roots data

--- utils/roots.py
+++ utils/roots.py
@@ -8,11 +8,14 @@ Lists of roots are stored in the data directory
 import numpy as np
 import csv 
 import numpy.polynomial.legendre as leg
+import numpy.polynomial.chebyshev as cheb
+
 from polydata import polydata
 
-# set max degree
+# set some global variables
 MAXD = polydata['MAXDEGREE']
 LEGF = polydata['LEGFILE']
+CHEBF = polydata['CHEBFILE']
 
 # function to genereate Legendre 
 def get_leg_roots():
@@ -30,3 +33,24 @@ def get_leg_roots():
         w.writerow([key,val])
 
     return 
+
+# function to genereate Chebyshev Roots 
+def get_cheb_roots():
+    roots = {}
+    # loop over all degrees up to MAXD
+    for i in range(0,MAXD):
+        c = np.zeros(MAXD)
+        c[i] = 1
+        r = cheb.chebroots(c)
+        roots.update({i:r})
+   
+    # save roots to file
+    w = csv.writer(open(CHEBF,"w"))
+    for key, val in roots.items():
+        w.writerow([key,val])
+
+    return 
+
+if (__name__ == '__main__'):
+    get_leg_roots()
+    get_cheb_roots()
